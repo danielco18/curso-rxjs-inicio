5af009687d07de86ff134f1a22e53f175278bf0d
Daniel Cardona <daniel.eco18@gmail.com>
2019-12-04 17:26:40 -0500

    Primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import { Observable } from 'rxjs';
 
+// No es muy usado
+// const obs$ = Observable.create()
 
-console.log('Hola Mundo!');
+// Subscribers
+// Los que están pendientes de las emisiones de un observable
+const obs$ = new Observable<string>(subs => {
+    subs.next('Hola');
+    subs.next('Mundo');
 
+    subs.next('Hola');
+    subs.next('Mundo');
+    
+    subs.complete();
+    // No emite por que ya se completo
+    subs.next('Mundo');
+});
 
-
+obs$.subscribe(console.log)
+console.log('-------')
+obs$.subscribe(console.log)
 
 
 
