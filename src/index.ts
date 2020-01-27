38a9d8b850f2a63fb9755aab0306a6e8f61ded7c
Daniel Cardona <daniel.eco18@gmail.com>
2020-01-27 17:53:50 -0500

    operator map

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,20 @@
-import { Observer, of, from } from 'rxjs';
+import { range, pipe, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// const range$ = range(1, 5)
 
 
-// of = toma argumentos y genera una secuencia
-// from = array, promise, iterable, observable
-
-// Observer
-const observer: Observer<any> = {
-    next: console.log,
-    error: console.error,
-    complete: () => console.info('Completado')
-}
-
-const generator = function*() {
-    yield 1;
-    yield 2;
-    yield 3;
-    yield 4;
-    yield 5;
-}
-
-const iterable = generator();
-
-from(iterable).subscribe(observer)
+// range$
+//     .pipe(
+//         map(n => n * 10)
+//     )
+// .subscribe(console.log)
 
 
-// const src$ = from([1, 2, 3, 4, 5])
-// const src$ = of(...[1, 2, 3, 4, 5])
-// const src$ = from('Daniel')
-// const src$ = from(fetch('https://api.github.com/users/danielco18'))
-
-// src$.subscribe(async resp => {
-//     const data = await resp.json()
-//     console.log(data);
-// })
-// src$.subscribe(observer)
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
+const keyupCode$ = keyup$
+    .pipe(
+        map(event => event.code)
+    )
+    
+keyupCode$.subscribe(code => console.log('map', code))
