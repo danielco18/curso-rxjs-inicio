6356d57574786e7d4a6af932ba70379d8c82f16f
Daniel Cardona <daniel.eco18@gmail.com>
2020-02-25 17:23:37 -0500

    auditTime operator

---
+++ src/time-operators/auditTime.ts
@@ -0,0 +1,12 @@
+import { fromEvent } from 'rxjs';
+import { auditTime, map, tap } from 'rxjs/operators';
+
+const click$ = fromEvent<MouseEvent>(document, 'click');
+
+click$
+    .pipe(
+        map(({ x }) => x),
+        tap((t) => console.log('tap', t)),
+        auditTime(2000),
+    )
+    .subscribe(console.log);
